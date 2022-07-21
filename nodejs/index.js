ee0924c66bbd59981432035e58179690b4a026db
Phạm Anh Hiếu <HIEUPA.B19CN254@stu.ptit.edu.vn>
2022-07-21 23:36:03 +0700

    update

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,11 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const userRoute = require("./controllers/routes/userRoutes");
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://hieu:hieu123456@hieu.zjelqqh.mongodb.net/?retryWrites=true&w=majority`
+    );
+    console.log("MongoDB Connected...");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+connectDB();
+
+app.use(bodyParser.json());
+app.use("/", userRoute);
+// app.get("/", (req, res) => {
+//   res.send("Hello World!?");
+// });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on http://localhost:${port}`);
+});
