96f3e2d21e85345599ca996d01ac7c74d649ccae
Phạm Anh Hiếu <HIEUPA.B19CN254@stu.ptit.edu.vn>
2022-08-18 15:45:08 +0700

    update validate, middleware for user

--- nodejs/routes/userRoutes.js
+++ nodejs/routes/userRoutes.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const UserController = require("../controllers/user.controller");
+const userMiddleware = require("../middleware/user.middleware");
 const userRouter = express.Router();
 
-userRouter.post("/", UserController.handleCreate);
+userRouter.post(
+  "/",
+  userMiddleware.checkRequired,
+  userMiddleware.checkValid,
+  userMiddleware.checkExist,
+  UserController.handleCreate
+);
 userRouter.get("/:id", UserController.handleGet);
 userRouter.delete("/:id", UserController.handleDelete);
 userRouter.put("/:id", UserController.handleUpdate);
-userRouter.post("/login", UserController.handleLogin);
+userRouter.post(
+  "/login",
+  userMiddleware.checkRequiredLogin,
+  userMiddleware.checkValid,
+  UserController.handleLogin
+);
 userRouter.post("/query-age-add/", UserController.handleQueryAgeAddress);
 userRouter.post("/query-role/", UserController.handleGetAllAtt);
 userRouter.post("/query-search/", UserController.handleSearch);
