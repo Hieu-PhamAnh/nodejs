d038ec9b3acc548aa2a582fb8326723d3ecc3ce7
Phạm Anh Hiếu <HIEUPA.B19CN254@stu.ptit.edu.vn>
2022-09-07 20:59:41 +0700

    fix token

--- nodejs/controllers/auth.controller.js
+++ nodejs/controllers/auth.controller.js
@@ -1,41 +1,42 @@
 const jwt = require("jsonwebtoken");
 const Token = require("../models/Token");
 
-const refreshToken = (req, res) => {
+const refreshToken = async (req, res) => {
   const { refreshToken } = req.body;
   try {
-    jwt.verify(
+    const response = await jwt.verify(
       refreshToken,
-      process.env.SECRET_KEY_REFRESH,
-      async (err, payload) => {
-        if (err) {
-          res.status(403).json({
-            message: "token khong hop le",
-            err: err,
-          });
-        }
-        const id = payload._id;
-        const accessTK = jwt.sign({ _id: id }, process.env.SECRET_KEY_ACCESS, {
-          expiresIn: parseInt(process.env.ACCESS_TOKEN_EXPIRE || 10) * 60,
-        });
-        const refreshTK = jwt.sign(
-          { _id: id },
-          process.env.SECRET_KEY_REFRESH,
-          {
-            expiresIn: parseInt(process.env.REFRESH_TOKEN_EXPIRE || 10) * 60,
-          }
-        );
-        const token = await Token.findOneAndUpdate(
-          { userID: id },
-          { token: refreshTK }
-        );
-        return res.status(200).json({
-          message: "refresh thanh cong",
-          accessToken: accessTK,
-          refreshToken: refreshTK,
-        });
-      }
+      process.env.SECRET_KEY_REFRESH
+      // async (err, payload) => {
+      //   if (err) {
+      //     res.status(403).json({
+      //       message: "token khong hop le",
+      //       err: err,
+      //     });
+      //   }
+      //   const id = payload._id;
+      //   const accessTK = jwt.sign({ _id: id }, process.env.SECRET_KEY_ACCESS, {
+      //     expiresIn: parseInt(process.env.ACCESS_TOKEN_EXPIRE || 10) * 60,
+      //   });
+      //   const refreshTK = jwt.sign(
+      //     { _id: id },
+      //     process.env.SECRET_KEY_REFRESH,
+      //     {
+      //       expiresIn: parseInt(process.env.REFRESH_TOKEN_EXPIRE || 10) * 60,
+      //     }
+      //   );
+      //   const token = await Token.findOneAndUpdate(
+      //     { userID: id },
+      //     { token: refreshTK }
+      //   );
+      //   return res.status(200).json({
+      //     message: "refresh thanh cong",
+      //     accessToken: accessTK,
+      //     refreshToken: refreshTK,
+      //   });
+      // }
     );
+    console.log(response.payload);
   } catch (error) {
     return res.status(500).json({
       message: error,
--- nodejs/controllers/user.controller.js
+++ nodejs/controllers/user.controller.js
@@ -291,25 +291,15 @@ const UserController = {
   },
   refreshToken: (req, res) => {
     const { refreshToken } = req.body;
-    try {
-      jwt.verify(
-        refreshToken,
-        process.env.SECRET_KEY_REFRESH,
-        (err, payload) => {
-          if (err) {
-            res.status(403).json({
-              message: "token khong hop le",
-              err: err,
-            });
-          }
-          console.log(payload);
-        }
-      );
-    } catch (error) {
-      return res.status(500).json({
-        message: error,
-      });
-    }
+    jwt.verify(refreshToken, process.env.SECRET_KEY_REFRESH, (err, payload) => {
+      if (err) {
+        res.status(403).json({
+          message: "token khong hop le",
+          err: err,
+        });
+      }
+      console.log(payload);
+    });
   },
 };
 module.exports = UserController;
